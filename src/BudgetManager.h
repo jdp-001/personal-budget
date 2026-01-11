209934c8a00541fb1c1648b92434293b3dca2cee
Jaroslaw Dolinski <jaroslaw.dolinski.programista@gmail.com>
2026-01-11 13:06:48 +0100

    Initialize empty BudgetManager interface

--- src/BudgetManager.h
+++ src/BudgetManager.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "DateMethods.h"
+
+class BudgetMenager
+{
+    DateMethods dateMethods;
+
+    struct DateRange
+    {
+        int startDate;
+        int endDate;
+    };
+
+public:
+    DateRange getCurrentMonthRange();
+    DateRange getPreviousMonthRange();
+};
